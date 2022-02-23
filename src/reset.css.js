6f0baf7b43a72cc05a8a9b39fae0042fc167f005
Jackalob <nike9712@gmail.com>
2022-02-24 00:55:47 +0800

    style: add reset css

---
+++ src/reset.css.js
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: inherit;
+    -webkit-font-smoothing: auto;
+    font-weight: inherit;
+    margin: 0;
+    outline: 0;
+    text-rendering: optimizeLegibility;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  body {
+    box-sizing: border-box;
+    width: 100%;
+    height: 100%;
+    overscroll-behavior-y: none;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  #root {
+    height: 100%;
+    width: 100%;
+  }
+
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
+
+  a:hover {
+    cursor: pointer;
+  }
+
+  textarea {
+    resize: none;
+  }
+  
+  ul, li {
+    list-style: none;
+  }
+`

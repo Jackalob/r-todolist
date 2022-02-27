a6a3853dc636d476b45ed74e441bfe089d7cc9b1
Jackalob <nike9712@gmail.com>
2022-02-27 19:18:07 +0800

    feat: mod CountBadge

--- src/components/CountBadge/index.js
+++ src/components/CountBadge/index.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from 'styled-components';
 
-export default function CountBadge() {
+const StyledBadge = styled.div`
+  height: 80%;
+  padding: 2px 8px;
+  background-color: #F3BA18;
+  color: #333;
+  border-radius: 20px;
+  font-size: 14px;
+`
+export default function CountBadge(props) {
   const countRef = React.useRef(0);
   return (
-    <span>{countRef.current++}</span>
+    <StyledBadge>
+      <span>{countRef.current++}</span>
+    </StyledBadge>
   )
 }
+
